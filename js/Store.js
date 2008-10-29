0322aa6d219c3fc3fafefc8f2c3ca6e82460f291
com.chinaos.snaill <com.chinaos.snaill@6c5ab64a-a35b-11dd-8805-4530818397e5>
2008-10-29 10:25:53 +0000

    增加MessagePanel 增加AddDirPanel 增加ContentPanel 修改网页整体布局

--- js/Store.js
+++ js/Store.js
@@ -9,9 +9,9 @@
 Ext.app.StorePanel = function() {
 
   Ext.app.StorePanel.superclass.constructor.call(this, {
-    width    : 600,
-    height   : 350,
- //    renderTo: 'view',
+    height   : 450,
+    autoWidth : true,
+  //  autoHeight : true,
     plain    : true,
     layout   : 'border',
     items    : [new Ext.app.StoreTree, new Ext.app.MainPanel()]
@@ -20,11 +20,16 @@ Ext.app.StorePanel = function() {
 
 Ext.extend(Ext.app.StorePanel, Ext.Panel, {} );
 
+Ext.BLANK_IMAGE_URL = '../../extjs/resources/images/default/s.gif';
+
 Ext.onReady(function(){
 
   Ext.QuickTips.init();
   Ext.state.Manager.setProvider( new Ext.state.SessionProvider( { state: Ext.appState } ) );
             
+  var msgpanel = new Ext.app.MessagePanel();
+  msgpanel.render('msgbox');
+  
   var panel = new Ext.app.StorePanel();
   panel.render('view');
 });
