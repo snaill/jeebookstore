ecff04a9569c5411b4139d737dc35696d892e80d
com.chinaos.snaill <com.chinaos.snaill@6c5ab64a-a35b-11dd-8805-4530818397e5>
2008-11-17 02:00:40 +0000

    实现StoreGridReader

--- js/StoreTree.js
+++ js/StoreTree.js
@@ -12,6 +12,8 @@ Ext.app.StoreTree = function() {
 		title       : 'Folders',
 		region      : 'west',
 		split       : true,
+		useArrows	: true,
+        animate		: true,
 		width       : 200,
 		collapsible : true,
 		margins     : '3 0 3 3',
@@ -21,10 +23,11 @@ Ext.app.StoreTree = function() {
        	}),
 		root 		: new Ext.tree.AsyncTreeNode({
 			id:'root',
-			text:'Store',
-			attributes : 'root/dirs.xml'	 
+			text:'Store'
 		})
 	});
+
+	this.root.expand();
 };
 
 Ext.extend(Ext.app.StoreTree, Ext.tree.TreePanel, {} );
