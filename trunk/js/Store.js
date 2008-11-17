7de3ab7e8113103f43c9cb74215080d5c728d347
com.chinaos.snaill <com.chinaos.snaill@6c5ab64a-a35b-11dd-8805-4530818397e5>
2008-11-17 02:00:40 +0000

    实现StoreGridReader

--- trunk/js/Store.js
+++ trunk/js/Store.js
@@ -8,28 +8,36 @@
 
 Ext.app.StorePanel = function() {
 
-  Ext.app.StorePanel.superclass.constructor.call(this, {
-    height   : 450,
-    autoWidth : true,
-  //  autoHeight : true,
-    plain    : true,
-    layout   : 'border',
-    items    : [new Ext.app.StoreTree, new Ext.app.MainPanel()]
-  });
+	this.tree = new Ext.app.StoreTree();
+	this.main = new Ext.app.MainPanel();
+	Ext.app.StorePanel.superclass.constructor.call(this, {
+		height   : 450,
+		autoWidth : true,
+		//  autoHeight : true,
+		plain    : true,
+		layout   : 'border',
+		items    : [this.tree, this.main]
+	});
+
+    this.tree.on('click', this.onSelectTreeNode, this );
 };
 
-Ext.extend(Ext.app.StorePanel, Ext.Panel, {} );
+Ext.extend(Ext.app.StorePanel, Ext.Panel, {
+	onSelectTreeNode : function( node )	{
+		this.main.grid.load( node.getPath() );
+	}
+} );
 
-Ext.BLANK_IMAGE_URL = '../../extjs/resources/images/default/s.gif';
+Ext.BLANK_IMAGE_URL = '../extjs/resources/images/default/s.gif';
 
 Ext.onReady(function(){
 
-  Ext.QuickTips.init();
-  Ext.state.Manager.setProvider( new Ext.state.SessionProvider( { state: Ext.appState } ) );
-            
-  var msgpanel = new Ext.app.MessagePanel();
-  msgpanel.render('msgbox');
-  
-  var panel = new Ext.app.StorePanel();
-  panel.render('view');
+	Ext.QuickTips.init();
+	Ext.state.Manager.setProvider( new Ext.state.SessionProvider( { state: Ext.appState } ) );
+			
+	var msgpanel = new Ext.app.MessagePanel();
+	msgpanel.render('msgbox');
+
+	var panel = new Ext.app.StorePanel();
+	panel.render('view');
 });
