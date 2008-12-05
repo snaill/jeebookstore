4b245018202c472ec742e53f02611d71e6a20d66
com.chinaos.snaill <com.chinaos.snaill@6c5ab64a-a35b-11dd-8805-4530818397e5>
2008-12-05 20:03:33 +0000

    移动登录面板到导航条 增加搜索功能

--- js/Navigatebar.js
+++ js/Navigatebar.js
@@ -10,28 +10,44 @@ Ext.app.Navigatebar = function() {
 
 	Ext.app.Navigatebar.superclass.constructor.call(this, {
 		id : 'Navigatebar_Id',
-		items : [' ',
-				new Ext.StatusBar({
-					id: 'Statusbar_Id',
-					defaultText: 'Jeebook Store'
-				}),'->',{
-					id:"btn_mainSite",
-					text: Ext.app.Resource.Toolbar.MainSite,
-					handler: this.onMainSite,
-					scope:this
-				}, '-', 
-				{
-					id:"btn_blogSite",
-					text:Ext.app.Resource.Toolbar.BlogSite,
-					handler: this.onBlogSite,
-					scope:this
-				}, '-', 
-				{
-					id:"btn_help",
-					text:Ext.app.Resource.Toolbar.Help,
-					handler: this.onHelp,
-					scope:this
-				}, ' ']
+		items : [' ', new Ext.form.Label({ text : 'Jeebook Store' }), ' ', ' ',
+			{
+				xtype: 'textfield',
+				hideLabel: true,	
+				emptyText: Ext.app.Resource.EmptyText.LoginMail,
+				name:'email',
+				width:150
+			}, new Ext.Button({
+				text : Ext.app.Resource.Button.Login,
+				handler : this.onLogin,
+				scope :	this
+			}), ' ',
+			new Ext.StatusBar({
+				id: 'Statusbar_Id',
+				defaultText: ''
+			}),'->',{
+				id:"btn_mainSite",
+				text: Ext.app.Resource.Toolbar.MainSite,
+				handler: this.onMainSite,
+				scope:this
+			}, '-', 
+			{
+				id:"btn_blogSite",
+				text:Ext.app.Resource.Toolbar.BlogSite,
+				handler: this.onBlogSite,
+				scope:this
+			}, '-', 
+			{
+				id:"btn_help",
+				text:Ext.app.Resource.Toolbar.Help,
+				handler: this.onHelp,
+				scope:this
+			}, ' '],
+		keys: [{    
+			key:Ext.EventObject.ENTER,   
+			fn: this.onLogin,   
+			scope:this
+		}] 
 	});
 };
 
@@ -52,6 +68,31 @@ Ext.extend(Ext.app.Navigatebar, Ext.Toolbar, {
 			iconCls:cls,
 			clear: true
         });
+	},
+	onLogin : function() {
+		var form = this.getForm();
+		if(form.isValid()){
+			form.submit({
+				url: './ashx/login.ashx',
+				waitMsg: Ext.app.Resource.WaitMsg.Login,
+				success: function(sender, o){
+					this.updateState( o.result.data );
+					this.setStatus( true, 'Login success!' );
+				},
+				failure : function(sender, o) {
+					Ext.app.StorePanel.getStatusbar().setStatus({
+						text:'Login failure!', 
+						iconCls:'',
+						clear: true
+					});						
+				},
+				scope : this
+			});
+		}
+	},
+	updateState : function( o )	{
+		Ext.app.UserState = o;
+		Ext.app.MainPanel.getObj().updateToolbar( o );
 	}
 });
 
@@ -59,3 +100,4 @@ Ext.app.Navigatebar.getObj = function() {
 	return Ext.getCmp('Navigatebar_Id');
 }
 
+Ext.app.UserState = null;
